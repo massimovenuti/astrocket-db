7b2955f4fab45dc41c9900ca5ec515dd16550848
lsaunier <lsaunier@gitlab.unistra.fr>
2021-02-06 11:37:02 +0100

    testStats.sql

---
+++ testStats.sql
@@ -0,0 +1,44 @@
+/*** Utilisateur de test ***/
+insert into users (idUser, username, pwd, email, role)
+values (NEXTVAL(s_users), 'test', 'pwd', 'email@email.email', 'U');
+    
+/* idUser not null */
+insert into stats values (NULL,0,0,0,0,0,0,0,0,0,0,0);
+
+/* nbPoints >= 0 */
+insert into stats values ((select idUser from users where username='test'),-1,0,0,0,0,0,0,0,0,0,0);
+/*** Le select permet de retrouver dynamiquement la valeur de l'utilisateur test, afin de ne pas avoir à changer manuellement cette valeur ***/
+    
+/* nbKills >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,-1,0,0,0,0,0,0,0,0,0);
+    
+/* nbAsteroids >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,-1,0,0,0,0,0,0,0,0);
+    
+/* nbDeaths >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,-1,0,0,0,0,0,0,0);
+    
+/* nbPowerUps >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,0,-1,0,0,0,0,0,0);
+    
+/* nbGames >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,0,0,-1,0,0,0,0,0);
+    
+/* nbWins >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,0,0,0,-1,0,0,0,0);
+
+/* maxKills >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,0,0,0,0,-1,0,0,0);
+
+/* maxPoints >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,0,0,0,0,0,-1,0,0);
+    
+/* maxPowersUps >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,0,0,0,0,0,0,-1,0);
+    
+/* maxDeaths >= 0 */
+insert into stats values ((select idUser from users where username='test'),0,0,0,0,0,0,0,0,0,0,-1);
+
+/*** Suppression utilisateur de test ***/
+delete from users where username='test';
+    
