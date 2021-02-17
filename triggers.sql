14fcec759de5080ae4fa6645acf6b91313301807
lsaunier <lsaunier@etu.unistra.fr>
2021-02-17 10:35:46 +0100

    Ajout des triggers delete et update sur stats + Tests à venir

--- triggers.sql
+++ triggers.sql
@@ -39,3 +39,63 @@ BEGIN
 END;
 //
 
+DELIMITER //
+CREATE OR REPLACE trigger u_stats
+    AFTER UPDATE
+    ON stats
+    FOR EACH ROW
+BEGIN 
+    DECLARE maxK,maxD,maxP,maxPU INTEGER; 
+    IF NEW.maxKills < OLD.maxKills THEN
+        SET maxK = OLD.maxKills;
+    ELSE
+        SET maxK = NEW.maxKills;
+    END IF;
+
+    IF NEW.maxPoints < OLD.maxPoints THEN
+        SET maxP = OLD.maxPoints;
+    ELSE
+        SET maxP = NEW.maxPoints;
+    END IF;
+
+    IF NEW.maxDeaths < OLD.maxDeaths THEN
+        SET maxD = OLD.maxDeaths;
+    ELSE
+        SET maxD = NEW.maxDeaths;
+    END IF;
+
+    IF NEW.maxPowerUps < OLD.maxPowerUps THEN
+        SET maxPU = OLD.maxPowerUps;
+    ELSE
+        SET maxPU = NEW.maxPowerUps;
+    END IF;
+
+    UPDATE stats 
+    SET
+        nbPoints = NEW.nbPoints + OLD.nbPoints, 
+        nbKills = NEW.nbKills + OLD.nbKills, 
+        nbAsteroids = NEW.nbAsteroids + OLD.nbAsteroids, 
+        nbDeaths = NEW.nbDeaths + OLD.nbDeaths, 
+        nbPowerUps = NEW.nbPowerUps + OLD.nbPowerUps, 
+        nbGames =  OLD.nbGames + 1,
+        nbWins = NEW.nbWins + OLD.nbWins, 
+        maxKills = maxK, 
+        maxPoints = maxP, 
+        maxPowerUps = maxPU, 
+        maxDeaths = maxD     
+    WHERE (idUser = OLD.idUser);
+
+END;
+//
+
+DELIMITER //
+CREATE OR REPLACE trigger d_stats
+    AFTER DELETE
+    ON stats
+    FOR EACH ROW
+BEGIN
+    INSERT INTO stats (idUser) VALUES (OLD.idUser);
+END;
+//
+
+
