d6ebb98099cb81826f8bf5526f952a106ea89051
SAUNIER LOIC <loic.saunier@etu.unistra.fr>
2021-04-06 14:55:57 +0000

    Update createEventsProcedures.sql => on n'enlève pas le token de l'utilisateur main

--- src/createEventsProcedures.sql
+++ src/createEventsProcedures.sql
@@ -2,7 +2,7 @@ DELIMITER //
 CREATE OR REPLACE PROCEDURE p_clean_tables()
 BEGIN
     DELETE FROM bans WHERE banEnd < NOW();
-    DELETE FROM tokens WHERE expirationDate < DATE_SUB(NOW(), INTERVAL 1 DAY);
+    DELETE FROM tokens WHERE expirationDate < DATE_SUB(NOW(), INTERVAL 1 DAY) AND idUser != (SELECT idUser FROM users WHERE         username = "Main");
 END;
 //
 
